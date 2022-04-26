c955e34a03af810b13f967c742331088ae19a6ad
Max-14 <mulyatalukona@gmail.com>
2022-04-26 17:59:34 +0300

    Add project files.

---
+++ Hostel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hostel.Startup))]
+namespace Hostel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
